ed5619f32c347eef0fb0a3d962de81132e84a110
Aymane Elmadidi <aymanelmadidi@gmail.com>
2023-01-03 10:21:58 +0100

    adding config files

---
+++ apollo-client.js
@@ -0,0 +1,31 @@
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+	uri: "http://localhost:3000/graphql",
+	credentials: "include",
+});
+
+const authLink = setContext((_, { headers }) => {
+	// get the authentication token from local storage if it exists
+	// const token = localStorage.getItem("token");
+	// const access_token =
+	return {
+		headers: {
+			...headers,
+		},
+	};
+});
+
+const client = new ApolloClient({
+	credentials: "include",
+	link: httpLink,
+	cache: new InMemoryCache(),
+});
+
+// const client = new ApolloClient({
+// 	uri: "http://localhost:3000/graphql",
+// 	cache: new InMemoryCache(),
+// });
+
+export default client;
