72c02bf391f26fd513e7dc60bffc11b3ec2f303e
Aymane Elmadidi <aymanelmadidi@gmail.com>
2023-01-03 10:31:28 +0100

    adding helpers folder

---
+++ helpers/countries.ts
@@ -0,0 +1,759 @@
+export let nationalities = [
+	{ label: "afghane", value: "Afghanistan" },
+	{ label: "sudafricaine", value: "Afrique du Sud" },
+	{ label: "albanaise", value: "Albanie" },
+	{ label: "algérienne", value: "Algérie" },
+	{ label: "allemande", value: "Allemagne" },
+	{ label: "andorrane", value: "Andorre" },
+	{ label: "angolaise", value: "Angola" },
+	{ label: "antiguayenne", value: "Antigua-et-Barbuda" },
+	{ label: "saoudienne", value: "Arabie saoudite" },
+	{
+		label: "argentine",
+		value: "Argentine",
+	},
+	{
+		label: "arménienne",
+		value: "Arménie",
+	},
+	{
+		label: "australienne",
+		value: "Australie",
+	},
+	{
+		label: "autrichienne",
+		value: "Autriche",
+	},
+	{
+		label: "azerbaïdjanaise",
+		value: "Azerbaïdjan",
+	},
+	{
+		label: "bahaméenne",
+		value: "Bahamas",
+	},
+	{
+		label: "bahreïnienne",
+		value: "Bahreïn",
+	},
+	{
+		label: "bangladaise",
+		value: "Bangladesh",
+	},
+	{
+		label: "barbadienne",
+		value: "Barbade",
+	},
+	{
+		label: "belge",
+		value: "Belgique",
+	},
+	{
+		label: "bélizienne",
+		value: "Belize",
+	},
+	{
+		label: "bhoutanaise",
+		value: "Bhoutan",
+	},
+	{
+		label: "birmane",
+		value: "Birmanie",
+	},
+	{
+		label: "biélorusse",
+		value: "Biélorussie",
+	},
+	{
+		label: "bolivienne",
+		value: "Bolivie",
+	},
+	{
+		label: "bosnienne",
+		value: "Bosnie-Herzégovine",
+	},
+	{
+		label: "botswanaise",
+		value: "Botswana",
+	},
+	{
+		label: "brunéienne",
+		value: "Brunei",
+	},
+	{
+		label: "brésilienne",
+		value: "Brésil",
+	},
+	{
+		label: "bulgare",
+		value: "Bulgarie",
+	},
+	{
+		label: "burkinabé",
+		value: "Burkina",
+	},
+	{
+		label: "burundaise",
+		value: "Burundi",
+	},
+	{
+		label: "béninoise",
+		value: "Bénin",
+	},
+	{
+		label: "cambodgienne",
+		value: "Cambodge",
+	},
+	{
+		label: "camerounaise",
+		value: "Cameroun",
+	},
+	{
+		label: "canadienne",
+		value: "Canada",
+	},
+	{
+		label: "cap-verdienne",
+		value: "Cap-Vert",
+	},
+	{
+		label: "centrafricaine",
+		value: "Centrafrique",
+	},
+	{
+		label: "chilienne",
+		value: "Chili",
+	},
+	{
+		label: "chinoise",
+		value: "Chine",
+	},
+	{
+		label: "chypriote",
+		value: "Chypre",
+	},
+	{
+		label: "colombienne",
+		value: "Colombie",
+	},
+	{
+		label: "comorienne",
+		value: "Comores",
+	},
+	{
+		label: "congolaise",
+		value: "Congo",
+	},
+	{
+		label: "nord-coréenne",
+		value: "Corée du Nord",
+	},
+	{
+		label: "sud-coréenne",
+		value: "Corée du Sud",
+	},
+	{
+		label: "costaricienne",
+		value: "Costa Rica",
+	},
+	{
+		label: "croate",
+		value: "Croatie",
+	},
+	{
+		label: "cubaine",
+		value: "Cuba",
+	},
+	{
+		label: "ivoirienne",
+		value: "Côte d'Ivoire",
+	},
+	{
+		label: "danoise",
+		value: "Danemark",
+	},
+	{
+		label: "djiboutienne",
+		value: "Djibouti",
+	},
+	{
+		label: "dominiquaise",
+		value: "Dominique",
+	},
+	{
+		label: "egyptienne",
+		value: "Egypte",
+	},
+	{
+		label: "emirienne",
+		value: "Emirats arabes unis",
+	},
+	{
+		label: "equatorienne",
+		value: "Equateur",
+	},
+	{
+		label: "erythréenne",
+		value: "Erythrée",
+	},
+	{
+		label: "espagnole",
+		value: "Espagne",
+	},
+	{
+		label: "estonienne",
+		value: "Estonie",
+	},
+	{
+		label: "américaine",
+		value: "Etats-Unis",
+	},
+	{
+		label: "ethiopienne",
+		value: "Ethiopie",
+	},
+	{
+		label: "fidjienne",
+		value: "Fidji",
+	},
+	{
+		label: "finlandaise",
+		value: "Finlande",
+	},
+	{
+		label: "française",
+		value: "France",
+	},
+	{
+		label: "gabonaise",
+		value: "Gabon",
+	},
+	{
+		label: "gambienne",
+		value: "Gambie",
+	},
+	{
+		label: "ghanéenne",
+		value: "Ghana",
+	},
+	{
+		label: "grenadienne",
+		value: "Grenade",
+	},
+	{
+		label: "grecque",
+		value: "Grèce",
+	},
+	{
+		label: "guatémaltèque",
+		value: "Guatemala",
+	},
+	{
+		label: "guinéenne",
+		value: "Guinée",
+	},
+	{
+		label: "equatoguinéenne",
+		value: "Guinée équatoriale",
+	},
+	{
+		label: "bissaoguinéenne",
+		value: "Guinée-Bissao",
+	},
+	{
+		label: "guyanienne",
+		value: "Guyana",
+	},
+	{
+		label: "géorgienne",
+		value: "Géorgie",
+	},
+	{
+		label: "haïtienne",
+		value: "Haïti",
+	},
+	{
+		label: "hondurienne",
+		value: "Honduras",
+	},
+	{
+		label: "hongroise",
+		value: "Hongrie",
+	},
+	{
+		label: "indienne",
+		value: "Inde",
+	},
+	{
+		label: "indonésienne",
+		value: "Indonésie",
+	},
+	{
+		label: "irakienne",
+		value: "Irak",
+	},
+	{
+		label: "iranienne",
+		value: "Iran",
+	},
+	{
+		label: "irlandaise",
+		value: "Irlande",
+	},
+	{
+		label: "islandaise",
+		value: "Islande",
+	},
+	{
+		label: "israélienne",
+		value: "Israël",
+	},
+	{
+		label: "italienne",
+		value: "Italie",
+	},
+	{
+		label: "jamaïquaine",
+		value: "Jamaïque",
+	},
+	{
+		label: "japonaise",
+		value: "Japon",
+	},
+	{
+		label: "jordanienne",
+		value: "Jordanie",
+	},
+	{
+		label: "palestinienne",
+		value: "Jérusalem - Territoires palestiniens",
+	},
+	{
+		label: "kazakhstanais",
+		value: "Kazakhstan",
+	},
+	{
+		label: "kényane",
+		value: "Kenya",
+	},
+	{
+		label: "kirghize",
+		value: "Kirghizstan",
+	},
+	{
+		label: "kiribatienne",
+		value: "Kiribati",
+	},
+	{
+		label: "kosovare",
+		value: "Kosovo",
+	},
+	{
+		label: "koweïtienne",
+		value: "Koweït",
+	},
+	{
+		label: "laotienne",
+		value: "Laos",
+	},
+	{
+		label: "lesothienne",
+		value: "Lesotho",
+	},
+	{
+		label: "lettone",
+		value: "Lettonie",
+	},
+	{
+		label: "libanaise",
+		value: "Liban",
+	},
+	{
+		label: "libérienne",
+		value: "Liberia",
+	},
+	{
+		label: "libyenne",
+		value: "Libye",
+	},
+	{
+		label: "liechtensteinoise",
+		value: "Liechtenstein",
+	},
+	{
+		label: "lituanienne",
+		value: "Lituanie",
+	},
+	{
+		label: "luxembourgeoise",
+		value: "Luxembourg",
+	},
+	{
+		label: "macédonienne",
+		value: "Macédoine",
+	},
+	{
+		label: "malgache",
+		value: "Madagascar",
+	},
+	{
+		label: "malaisienne",
+		value: "Malaisie",
+	},
+	{
+		label: "malawienne",
+		value: "Malawi",
+	},
+	{
+		label: "maldivienne",
+		value: "Maldives",
+	},
+	{
+		label: "malienne",
+		value: "Mali",
+	},
+	{
+		label: "maltaise",
+		value: "Malte",
+	},
+	{
+		label: "marocaine",
+		value: "Maroc",
+	},
+	{
+		label: "marshallaise",
+		value: "Marshall",
+	},
+	{
+		label: "mauricienne",
+		value: "Maurice",
+	},
+	{
+		label: "mauritanienne",
+		value: "Mauritanie",
+	},
+	{
+		label: "mexicaine",
+		value: "Mexique",
+	},
+	{
+		label: "micronésienne",
+		value: "Micronésie",
+	},
+	{
+		label: "moldave",
+		value: "Moldavie",
+	},
+	{
+		label: "monégasque",
+		value: "Monaco",
+	},
+	{
+		label: "mongole",
+		value: "Mongolie",
+	},
+	{
+		label: "monténégrine",
+		value: "Monténégro",
+	},
+	{
+		label: "mozambicaine",
+		value: "Mozambique",
+	},
+	{
+		label: "namibienne",
+		value: "Namibie",
+	},
+	{
+		label: "nauruane",
+		value: "Nauru",
+	},
+	{
+		label: "nicaraguayenne",
+		value: "Nicaragua",
+	},
+	{
+		label: "nigérienne",
+		value: "Niger",
+	},
+	{
+		label: "nigériane",
+		value: "Nigeria",
+	},
+	{
+		label: "norvégienne",
+		value: "Norvège",
+	},
+	{
+		label: "néo-zélandaise",
+		value: "Nouvelle-Zélande",
+	},
+	{
+		label: "népalaise",
+		value: "Népal",
+	},
+	{
+		label: "omanaise",
+		value: "Oman",
+	},
+	{
+		label: "ougandaise",
+		value: "Ouganda",
+	},
+	{
+		label: "ouzbèke",
+		value: "Ouzbékistan",
+	},
+	{
+		label: "pakistanaise",
+		value: "Pakistan",
+	},
+	{
+		label: "palaoise",
+		value: "Palaos",
+	},
+	{
+		label: "panaméenne",
+		value: "Panama",
+	},
+	{
+		label: "papouasienne",
+		value: "Papouasie-Nouvelle-Guinée",
+	},
+	{
+		label: "paraguayenne",
+		value: "Paraguay",
+	},
+	{
+		label: "néerlandaise",
+		value: "Pays-Bas",
+	},
+	{
+		label: "philippine",
+		value: "Philippines",
+	},
+	{
+		label: "polonaise",
+		value: "Pologne",
+	},
+	{
+		label: "portugaise",
+		value: "Portugal",
+	},
+	{
+		label: "péruvienne",
+		value: "Pérou",
+	},
+	{
+		label: "qatarienne",
+		value: "Qatar",
+	},
+	{
+		label: "roumaine",
+		value: "Roumanie",
+	},
+	{
+		label: "britannique",
+		value: "Royaume-Uni",
+	},
+	{
+		label: "russe",
+		value: "Russie",
+	},
+	{
+		label: "rwandaise",
+		value: "Rwanda",
+	},
+	{
+		label: "dominicaine",
+		value: "République dominicaine",
+	},
+	{
+		label: "congolaise (RDC)",
+		value: "République démocratique du Congo",
+	},
+	{
+		label: "tchèque",
+		value: "République tchèque",
+	},
+	{
+		label: "christophienne",
+		value: "Saint-Christophe-et-Niévès",
+	},
+	{
+		label: "marinaise",
+		value: "Saint-Marin",
+	},
+	{
+		label: null,
+		value: "Saint-Siège",
+	},
+	{
+		label: "vincentaise",
+		value: "Saint-Vincent-et-les Grenadines",
+	},
+	{
+		label: "lucienne",
+		value: "Sainte-Lucie",
+	},
+	{
+		label: "salomonaise",
+		value: "Salomon",
+	},
+	{
+		label: "salvadorienne",
+		value: "Salvador",
+	},
+	{
+		label: "samoene",
+		value: "Samoa",
+	},
+	{
+		label: "santoméenne",
+		value: "Sao Tomé-et-Principe",
+	},
+	{
+		label: "serbe",
+		value: "Serbie",
+	},
+	{
+		label: "seychelloise",
+		value: "Seychelles",
+	},
+	{
+		label: "sierraléonaise",
+		value: "Sierra Leone",
+	},
+	{
+		label: "singapourienne",
+		value: "Singapour",
+	},
+	{
+		label: "slovaque",
+		value: "Slovaquie",
+	},
+	{
+		label: "slovène",
+		value: "Slovénie",
+	},
+	{
+		label: "somalienne",
+		value: "Somalie",
+	},
+	{
+		label: "soudanaise",
+		value: "Soudan",
+	},
+	{
+		label: "srilankaise",
+		value: "Sri Lanka",
+	},
+	{
+		label: "suisse",
+		value: "Suisse",
+	},
+	{
+		label: "surinamaise",
+		value: "Suriname",
+	},
+	{
+		label: "suédoise",
+		value: "Suède",
+	},
+	{
+		label: "swazie",
+		value: "Swaziland",
+	},
+	{
+		label: "syrienne",
+		value: "Syrie",
+	},
+	{
+		label: "sénégalaise",
+		value: "Sénégal",
+	},
+	{
+		label: "tadjike",
+		value: "Tadjikistan",
+	},
+	{
+		label: "tanzanienne",
+		value: "Tanzanie",
+	},
+	{
+		label: "taïwanaise",
+		value: "Taïwan",
+	},
+	{
+		label: "tchadienne",
+		value: "Tchad",
+	},
+	{
+		label: "thaïlandaise",
+		value: "Thaïlande",
+	},
+	{
+		label: "timoraise",
+		value: "Timor oriental",
+	},
+	{
+		label: "togolaise",
+		value: "Togo",
+	},
+	{
+		label: "tongienne",
+		value: "Tonga",
+	},
+	{
+		label: "trinidadienne",
+		value: "Trinité-et-Tobago",
+	},
+	{
+		label: "tunisienne",
+		value: "Tunisie",
+	},
+	{
+		label: "turkmène",
+		value: "Turkménistan",
+	},
+	{
+		label: "turque",
+		value: "Turquie",
+	},
+	{
+		label: "tuvaluane",
+		value: "Tuvalu",
+	},
+	{
+		label: "ukrainienne",
+		value: "Ukraine",
+	},
+	{
+		label: "uruguayenne",
+		value: "Uruguay",
+	},
+	{
+		label: "vanuataise",
+		value: "Vanuatu",
+	},
+	{
+		label: "vénézuélienne",
+		value: "Venezuela",
+	},
+	{
+		label: "vietnamienne",
+		value: "Viêt Nam",
+	},
+	{
+		label: "yéménite",
+		value: "Yémen",
+	},
+	{
+		label: "zambienne",
+		value: "Zambie",
+	},
+	{
+		label: "zimbabwéenne",
+		value: "Zimbabwe",
+	},
+];
