3dbc1b5b42933b539fcade0c9425f80ff5ff8cf5
Aymane Elmadidi <aymanelmadidi@gmail.com>
2023-01-30 11:56:11 +0100

    ajout catalogue

---
+++ pages/catalogue/categories.tsx
@@ -0,0 +1,558 @@
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import {
+	Table,
+	Checkbox,
+	createStyles,
+	UnstyledButton,
+	Group,
+	Text,
+	Center,
+	Select,
+	TextInput,
+	LoadingOverlay,
+	Accordion,
+} from "@mantine/core";
+import { keys } from "@mantine/utils";
+import { gql, useLazyQuery, useMutation, useQuery } from "@apollo/client";
+import "dayjs/locale/fr";
+// import client from "../apollo-client";
+// import SellersBar from "../components/SellersBar";
+// import { useSellers } from "../hooks/useSellerData";
+import {
+	IconSelector,
+	IconChevronDown,
+	IconChevronUp,
+	IconSearch,
+	IconCirclePlus,
+} from "@tabler/icons";
+import { DateRangePicker, DateRangePickerValue } from "@mantine/dates";
+// import { useRouter } from "next/router";
+import dayjs from "dayjs";
+import { showNotification } from "@mantine/notifications";
+import { openConfirmModal } from "@mantine/modals";
+import {
+	RowData,
+	ThProps,
+	sortData,
+	getStartAndEndFromRange,
+} from "../../utils/filtersUtils";
+
+const useStyles = createStyles((theme) => ({
+	th: {
+		padding: "0 !important",
+	},
+
+	control: {
+		width: "100%",
+		padding: `${theme.spacing.xs}px ${theme.spacing.md}px`,
+
+		"&:hover": {
+			backgroundColor:
+				theme.colorScheme === "dark"
+					? theme.colors.dark[6]
+					: theme.colors.gray[0],
+		},
+	},
+
+	icon: {
+		width: 21,
+		height: 21,
+		borderRadius: 21,
+	},
+}));
+
+function Th({ children, reversed, sorted, onSort, tailwind = "" }: ThProps) {
+	const { classes } = useStyles();
+	const Icon = sorted
+		? reversed
+			? IconChevronUp
+			: IconChevronDown
+		: IconSelector;
+	return (
+		<th className={`${classes.th} ${tailwind}`}>
+			<UnstyledButton onClick={onSort} className={classes.control}>
+				<Group position="apart">
+					<Text weight={500} size="sm">
+						{children}
+					</Text>
+					<Center className={classes.icon}>
+						<Icon size={14} stroke={1.5} />
+					</Center>
+				</Group>
+			</UnstyledButton>
+		</th>
+	);
+}
+
+function MyAcc({ children }: any) {
+	return (
+		<Accordion>
+			<tr>{children}</tr>
+		</Accordion>
+	);
+}
+
+export default function Demo({ opened }: any) {
+	const [selection, setSelection] = useState([]);
+	const [search, setSearch] = useState("");
+	const [sortBy, setSortBy] = useState<keyof RowData | null>(null);
+	const [reverseSortDirection, setReverseSortDirection] = useState(false);
+	const [emailToSearch, setEmailToSearch] = useState("");
+	const [nomEntrepriseToSearch, setNomEntrepriseToSearch] = useState("");
+	const [pseudoToSearch, setPseudoToSearch] = useState("");
+	// const [startDateToSearch, setStartDateToSearch] = useState("");
+	// const [endDateToSearch, setEndDateToSearch] = useState("");
+	const [statut, setStatut] = useState("");
+	const [rangeValue, setRangeValue] = useState<DateRangePickerValue>([
+		null,
+		null,
+	]);
+	const [isOpened, setIsOpened] = useState(opened);
+	const [changedByBulkIds, setChangedByBulkIds] = useState<any>([]);
+	// const router = useRouter();
+
+	const [list, setList] = useState<any>([]);
+
+	useEffect(() => {
+		setIsOpened(opened);
+	}, [opened]);
+
+	// const [updateStatut, statutUpdateResult] = useMutation(UPDATE_BUYER_STATUT);
+
+	// const openModal = (e: any) => {
+	// 	return openConfirmModal({
+	// 		className: "mt-[200px]",
+	// 		confirmProps: {
+	// 			className: "bg-green-500 hover:bg-green-600 rounded-2xl",
+	// 		},
+	// 		cancelProps: {
+	// 			className: "rounded-2xl",
+	// 		},
+	// 		title: "Veuillez confirmer le changement de statut",
+	// 		children: (
+	// 			<p>
+	// 				{e === "actif" ? (
+	// 					<p>
+	// 						Voulez vous rendre ces utilisateurs{" "}
+	// 						<span className="text-green-500">Actif</span> ?
+	// 					</p>
+	// 				) : e === "inactif" ? (
+	// 					<p>
+	// 						Voulez vous rendre ces utilisateurs{" "}
+	// 						<span className="text-red-400">Inactif</span> ?
+	// 					</p>
+	// 				) : (
+	// 					<p>
+	// 						Voulez vous rendre ces utilisateurs{" "}
+	// 						<span className="text-red-400">Archivé</span> ?
+	// 					</p>
+	// 				)}
+	// 			</p>
+	// 		),
+	// 		labels: { confirm: "Confirmer", cancel: "Abandonner" },
+	// 		onCancel: () => {
+	// 			setStatut(statut);
+	// 		},
+	// 		onConfirm: async () => {
+	// 			try {
+	// 				let s: any;
+	// 				let test: any;
+	// 				let arr: number[] = [];
+	// 				if (e === "actif" || e === "inactif") {
+	// 					for (s of selection) {
+	// 						await updateStatut({
+	// 							variables: {
+	// 								_id: s,
+	// 								updateBuyerInput: {
+	// 									statut: e,
+	// 								},
+	// 							},
+	// 						});
+	// 					}
+	// 				} else {
+	// 					for (s of selection) {
+	// 						await updateStatut({
+	// 							variables: {
+	// 								_id: s,
+	// 								updateBuyerInput: {
+	// 									isArchived: true,
+	// 								},
+	// 							},
+	// 						});
+	// 						arr.push(s);
+	// 						// setList({ sellers: test });
+	// 						// console.log("test: ", test);
+	// 					}
+	// 					// console.log("arr: ", arr);
+	// 					test = list.buyers.filter(
+	// 						(buyer: any) => !arr.includes(buyer.userId)
+	// 					);
+
+	// 					setList({ buyers: test });
+	// 					// setList({ sellers: test });
+	// 				}
+	// 				setChangedByBulkIds(selection);
+	// 				if (e !== "archive") setStatut(() => e);
+	// 				setSelection([]);
+	// 				showNotification({
+	// 					title: `${
+	// 						e !== "archive" ? "Changement de multiple statut" : "Archivage"
+	// 					}`,
+	// 					message: `${
+	// 						e !== "archive"
+	// 							? "Statuts changé avec success"
+	// 							: "Archivage fait avec success"
+	// 					}`,
+	// 					color: "green",
+	// 					autoClose: 5000,
+	// 					bottom: "630px",
+	// 					// top: "0px",
+	// 					// classNames: {
+	// 					// 	root: "translate-y-[-500px]",
+	// 					// },
+	// 				});
+	// 			} catch (e) {
+	// 				alert(e);
+	// 				showNotification({
+	// 					title: "Changement de statut impossible",
+	// 					message: "Une erreur s'est produite",
+	// 					color: "red",
+	// 					autoClose: 5000,
+	// 					bottom: "630px",
+	// 				});
+	// 			}
+	// 		},
+	// 	});
+	// };
+
+	// const { error, loading, data } = useQuery(ALL_BUYERS, {
+	// 	onCompleted: setList,
+	// 	fetchPolicy: "no-cache",
+	// });
+	// const [getEmailsBySearch, results] = useLazyQuery(GET_BUYERS_BY_OC);
+
+	// if (loading) {
+	// 	return (
+	// 		<div className="flex justify-center">
+	// 			<LoadingOverlay visible />;
+	// 		</div>
+	// 	);
+	// }
+
+	// if (error) {
+	// 	// router.push("/login");
+
+	// 	return <div>{error.message}</div>;
+	// }
+
+	// let buyersData = data?.buyers;
+	// let buyers = [];
+	// if (results.data) {
+	// 	buyersData = results.data.buyersOcc;
+	// }
+
+	// const setSorting = (field: keyof RowData) => {
+	// 	const reversed = field === sortBy ? !reverseSortDirection : false;
+	// 	setReverseSortDirection(reversed);
+	// 	setSortBy(field);
+	// 	buyersData = sortData(buyersData, { sortBy: field, reversed, search });
+	// 	setList({ sellers: buyersData });
+	// };
+
+	// const toggleRow = (id: string) =>
+	// 	setSelection((current: any) =>
+	// 		current.includes(id)
+	// 			? current.filter((item: any) => item !== id)
+	// 			: [...current, id]
+	// 	);
+	// const toggleAll = () =>
+	// 	setSelection((current) =>
+	// 		current.length === buyersData.length
+	// 			? []
+	// 			: buyersData.map((item: any) => item.userId)
+	// 	);
+
+	// const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+	// 	const { value } = event.currentTarget;
+	// 	setSearch(value);
+	// 	buyersData = sortData(buyersData, {
+	// 		sortBy,
+	// 		reversed: reverseSortDirection,
+	// 		search: value,
+	// 	});
+	// 	console.log(buyersData);
+	// 	setList({ buyers: buyersData });
+	// };
+
+	// console.log("buyers", list.buyers);
+
+	// buyers = list.buyers.map((user: any) => {
+	// 	console.log("isArchived: ", user);
+	// 	if (!user.isArchived) {
+	// 		return (
+	// 			<BuyersBar
+	// 				key={user.email}
+	// 				user={user}
+	// 				selection={selection}
+	// 				toggleRow={toggleRow}
+	// 				statut={statut === "" ? user.statut : statut}
+	// 				ids={changedByBulkIds}
+	// 				setList={setList}
+	// 				list={list}
+	// 			/>
+	// 		);
+	// 	}
+	// });
+
+	// console.log("opened: ", opened);
+
+	return (
+		<div className={`${isOpened ? "lg:ml-[15%]" : ""} lg:m-auto lg:w-[85%]`}>
+			{/* <div className="lg:w-[85%] lg:m-auto"> */}
+			<div className="flex gap-3">
+				<p className="text-2xl mb-3 font-semibold">Acheteurs</p>
+				<Link href={"utilisateurs/acheteurs/ajouter-acheteur"}>
+					<IconCirclePlus size={35} />
+				</Link>
+			</div>
+			{/* <form className="">
+				<div className="flex flex-col px-5 pt-5 py-2 bg-white rounded-2xl shadow-sm mb-2">
+					<div className="flex gap-2">
+						<TextInput
+							classNames={{
+								// input: "w-[250px] rounded-2xl",
+								input: " rounded-2xl lg:w-[250px]",
+							}}
+							placeholder="E-mail"
+							// placeholder="Search by E-mail"
+							mb="md"
+							icon={<IconSearch size={14} stroke={1.5} />}
+							value={emailToSearch}
+							autoComplete="off"
+							onChange={(e) => setEmailToSearch(e.currentTarget.value)}
+						/>
+						<TextInput
+							classNames={{
+								input: " rounded-2xl lg:w-[280px]",
+								// input: "w-[280px] rounded-2xl",
+							}}
+							// placeholder="Search by nom de société"
+							placeholder="Société"
+							mb="md"
+							icon={<IconSearch size={14} stroke={1.5} />}
+							value={nomEntrepriseToSearch}
+							autoComplete="off"
+							onChange={(e) => setNomEntrepriseToSearch(e.currentTarget.value)}
+						/>
+					</div>
+					<div className="flex flex-col lg:flex-row lg:justify-between items-start">
+						<div className="flex gap-2">
+							<TextInput
+								classNames={{
+									// input: "w-[250px] rounded-2xl",
+									root: "basis-3/5",
+									input: "rounded-2xl lg:w-[250px]",
+								}}
+								// placeholder="Search by pseudo"
+								placeholder="Pseudo"
+								mb="md"
+								icon={<IconSearch size={14} stroke={1.5} />}
+								value={pseudoToSearch}
+								onChange={(e) => setPseudoToSearch(e.currentTarget.value)}
+							/>
+							<DateRangePicker
+								// label="Book hotel"
+								locale="fr"
+								classNames={{
+									// input: "w-[350px] rounded-2xl",
+									root: "w-full",
+									input: "rounded-2xl lg:w-[350px]",
+								}}
+								placeholder="Date d’enregistrement min - max"
+								// placeholder="Pick dates range"
+								value={rangeValue}
+								onChange={setRangeValue}
+								maxDate={dayjs(new Date()).toDate()}
+							/>
+						</div>
+						<button
+							type="submit"
+							className="bg-green-600 text-white text-xs hover:bg-green-500 px-[30px] py-3 rounded-2xl"
+							onClick={async (e) => {
+								e.preventDefault();
+								setSearch("");
+								console.log("rangeValue:", rangeValue);
+								let ranges = getStartAndEndFromRange(rangeValue);
+								console.log("ranges", ranges);
+								const datax = await getEmailsBySearch({
+									variables: {
+										email: emailToSearch,
+										nomEntreprise: nomEntrepriseToSearch,
+										pseudo: pseudoToSearch,
+										startDate: ranges[0],
+										endDate: ranges[1],
+									},
+								});
+								// console.log("ranges: ", ranges[0]);
+								// console.log("ranges: ", ranges[1]);
+								// console.log(datax.data.sellersOcc);
+								// setSortedData(datax.data.sellersOcc);
+								// console.log("search by dates data: ", datax.data.sellersOcc);
+								setList({ buyers: datax.data.buyersOcc });
+							}}
+						>
+							Rechercher
+						</button>
+					</div>
+				</div>
+			</form> */}
+			{/* <div className="flex gap-1 lg:justify-between mt-3">
+				<Select
+					classNames={{
+						input:
+							"rounded-2xl placeholder:font-bold border-green-600 border-2 shadow-xl",
+					}}
+					disabled={selection.length === 0}
+					rightSection={
+						<IconChevronDown
+							size={14}
+							color={"black"}
+							// style={{ marginRight: "10px" }}
+						/>
+					}
+					rightSectionWidth={30}
+					placeholder="Actions"
+					// defaultValue={user.statut}
+					onChange={(e) => {
+						openModal(e); // this actually works just need to update the ui
+					}}
+					data={[
+						{ value: "archive", label: "Archiver" },
+						{ value: "actif", label: "activer" },
+						{ value: "inactif", label: "desactiver" },
+					]}
+				/>
+				<TextInput
+					placeholder="Recherche"
+					classNames={{
+						input: "rounded-2xl w-[200px] lg:w-[250px]",
+					}}
+					mb="md"
+					icon={<IconSearch size={14} stroke={1.5} />}
+					value={search}
+					onChange={handleSearchChange}
+					type="text"
+				/>
+			</div> */}
+			{/* {results.loading ? (
+				<div className="flex justify-center">
+					<LoadingOverlay
+						visible
+						loaderProps={{ size: "xl", color: "green", variant: "bars" }}
+					/>
+				</div>
+			) : ( */}
+			<div className="bg-white flex flex-col items-start shadow-sm rounded-xl px-4 py-4">
+				<table className="table-auto">
+					<thead>
+						<tr>
+							<th>Song</th>
+							<th>Artist</th>
+							<th>Year</th>
+						</tr>
+					</thead>
+					<tbody>
+						<Accordion>
+							<td>
+								<td>test</td>
+								<td>test</td>
+								<td>test</td>
+							</td>
+							<td>
+								<td>test</td>
+								<td>test</td>
+								<td>test</td>
+							</td>
+							<td>
+								<td>test</td>
+								<td>test</td>
+								<td>test</td>
+							</td>
+							<td></td>
+						</Accordion>
+					</tbody>
+				</table>
+				{/* <div className="flex justify-between w-full">
+					<div>
+						<Checkbox />
+					</div>
+					<div>
+						<span>Position</span>
+					</div>
+					<div>
+						<span>Nom categorie</span>
+					</div>
+					<div>
+						<span>Produits</span>
+					</div>
+					<div>
+						<span>Produits actif</span>
+					</div>
+					<div>
+						<span>Statut</span>
+					</div>
+					<div>
+						<span>Actions</span>
+					</div>
+				</div> */}
+				{/* <div className="flex justify-start w-full">
+					<Accordion className="w-full -translate-x-5" chevronPosition="left">
+						<Accordion.Item value="customization">
+							<Accordion.Control>
+								<div className="flex justify-between">
+									<div className="ml-[110px]">test</div>
+									<div>test</div>
+									<div>test</div>
+									<div>test</div>
+									<div>test</div>
+									<div>test</div>
+								</div>
+							</Accordion.Control>
+							<Accordion.Panel>test</Accordion.Panel>
+						</Accordion.Item>
+					</Accordion>
+				</div> */}
+			</div>
+		</div>
+	);
+}
+
+// export async function getStaticProps() {
+// 	// const results = useSellers();
+// 	// console.log("results: ", results);
+// 	const { data } = await client.query({
+// 		query: gql`
+// 			query Sellers {
+// 				sellers {
+// 					_id
+// 					email
+// 					nomEntreprise
+// 					numeroSiret
+// 					statut_moderation
+// 					typeVendeur
+// 					typeCompte
+// 					created_at
+// 					statut
+// 					pseudo
+// 				}
+// 			}
+// 		`,
+// 	});
+
+// 	return {
+// 		props: {
+// 			users: data.sellers,
+// 		},
+// 	};
+// }
